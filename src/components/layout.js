a11754958e85787b4e1b47e016c2fb533c3870d9
Abhijith T <abhijiththayad111@gmail.com>
2020-08-14 00:38:55 +0530

    added footer component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,36 +1,17 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import Footer from './Reusable/Footer';
 
 import "./bootstrap.min.css"
 import "./layout.css"
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
-  return (
-    <div>
-      
-        <main>{children}</main>
-        
-      </div>
-  )
-}
+const Layout = ({children}) => (
+  <>
+  {children}
+  <Footer />
+  </>
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
