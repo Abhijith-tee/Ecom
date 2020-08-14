feb04719468775458fb8f87ead8e95b2fb68d0cf
Abhijith T <abhijiththayad111@gmail.com>
2020-08-14 23:59:56 +0530

    added navbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,12 +2,14 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import Footer from './Reusable/Footer';
+import Navbar from './Reusable/Navbar';
 
 import "./bootstrap.min.css"
 import "./layout.css"
 
 const Layout = ({children}) => (
   <>
+  <Navbar />
   {children}
   <Footer />
   </>
